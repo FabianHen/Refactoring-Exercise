97f84f5ebf3d2f5c537096ea4509e519c67b53f4
Fabian H. <hensslerfabian@gmail.com>
2026-02-04 16:23:09 +0100

    implement delegates in tennis project

--- Tennis/TennisGame3.java
+++ Tennis/TennisGame3.java
@@ -1,37 +1,18 @@
 package tennis;
 
 public class TennisGame3 implements TennisGame {
-    
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    private final TennisGame1 delegate;
+
+    public TennisGame3(String player1Name, String player2Name) {
+        delegate = new TennisGame1(player1Name, player2Name);
     }
 
-    public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
-        }
-    }
-    
-    public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            this.p1 += 1;
-        else
-            this.p2 += 1;
-        
+    public String getScore(){
+        return delegate.getScore();
     }
 
+    public void wonPoint(String player){
+        delegate.wonPoint(player);
+    }
 }
